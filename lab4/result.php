5fbcfb8ce3fa8cfb847cac094122a91dc02ae30c
Martin Frost <frost@ceri.se>
2013-02-07 16:11:19 +0100

    working home search

---
+++ lab4/result.php
@@ -0,0 +1,88 @@
+<?php
+  function generate_sort_link($order_param) {
+    $query_string = $_GET;
+    $query_string['order_by'] = $order_param;
+    return "?" . http_build_query($query_string);
+  }
+
+  setcookie("bostadssok", serialize($_GET));
+
+  $ajax_request = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ;
+
+  $mysqli = new mysqli("localhost", "root", "", "intnet_lab4");
+
+  if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+
+  $conditions = array();
+
+  if($_GET['lan'] != "") {
+    array_push($conditions, "lan = '" . $_GET['lan'] . "'");
+  }
+  if($_GET['rum'] != "") {
+    array_push($conditions, "rum = " . $_GET['rum']);
+  }
+  if($_GET['objekttyp'] != "") {
+    array_push($conditions, "objekttyp = '" . $_GET['objekttyp'] . "'");
+  }
+
+  $query_string = "select * from bostader"; 
+  if(!empty($conditions)) {
+    $query_string = $query_string . " where " . implode(" and ", $conditions);
+  }
+
+  if(array_key_exists('order_by', $_GET) && $_GET['order_by'] != "") {
+    $query_string = $query_string . " order by " . $_GET['order_by'] . " asc";
+  } else {
+    $query_string = $query_string . " order by pris asc";
+  }
+
+  $query_string = $query_string . ";";
+
+  $search_results = $mysqli->query($query_string);
+  
+?>
+
+<?php
+if(!$ajax_request) {
+  echo "<html><head><title>Bostadssökningsresultat</title></head><body>";
+}
+?>
+    <h1>Resultat</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Län</th>
+          <th>Objekttyp</th>
+          <th>Adress</th>
+          <th><a href="<?php echo generate_sort_link('area') ?>">Area</a></th>
+          <th><a href="<?php echo generate_sort_link('rum') ?>">Rum</a></th>
+          <th><a href="<?php echo generate_sort_link('pris') ?>">Pris</a></th>
+          <th><a href="<?php echo generate_sort_link('avgift') ?>">Avgift</a></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php 
+          $search_results->data_seek(0);
+          while($row = $search_results->fetch_assoc()) {
+            echo "<tr>\n";
+            echo "<td>". $row['lan'] ."</td>\n";
+            echo "<td>". $row['objekttyp'] ."</td>\n";
+            echo "<td>". $row['adress'] ."</td>\n";
+            echo "<td>". $row['area'] ."</td>\n";
+            echo "<td>". $row['rum'] ."</td>\n";
+            echo "<td>". $row['pris'] ."</td>\n";
+            echo "<td>". $row['avgift'] ."</td>\n";
+            echo "</tr>\n";
+          }  
+        ?>
+      </tbody>
+    </table>
+<?php
+  if(!$ajax_request) {
+    echo "</body></html>";
+  }
+?>
+
---
+++ lab4/search.php
@@ -0,0 +1,104 @@
+<?php
+  $stored_search_params = unserialize($_COOKIE['bostadssok']);
+  
+  $mysqli = new mysqli("localhost", "root", "", "intnet_lab4");
+
+  if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+
+  $county = $mysqli->query("select distinct lan from bostader");
+  $no_rooms = $mysqli->query("select distinct rum from bostader");
+  $object_type = $mysqli->query("select distinct objekttyp from bostader");
+
+?>
+
+<html>
+  <head>
+    <title>Bostadssök</title>
+    <script type="text/javascript" src="jquery.js"></script>
+  </head>
+  <body>
+    <h1>Bostadssök</h1>
+    <div>
+      <form action="result.php" method="get" id="search_form">
+        <p>
+          <label for="lan"><span>Län</span>
+            <select id="lan" name="lan">
+              <option value=""></option>
+              <?php 
+                $county->data_seek(0);
+                while($row = $county->fetch_assoc()) {
+                  if($stored_search_params['lan'] == $row['lan']) {
+                    echo "<option value=\"". $row["lan"] ."\" selected=\"selected\">". $row["lan"] ."</option>";
+                  } else {
+                    echo "<option value=\"". $row["lan"] ."\">". $row["lan"] ."</option>";
+                  }
+                }
+              ?>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label for="rum"><span>Antal rum</span>
+            <select id="rum" name="rum">
+              <option value=""></option>
+              <?php 
+                $no_rooms->data_seek(0);
+                while($row = $no_rooms->fetch_assoc()) {
+                  if($stored_search_params['rum'] == $row['rum']) {
+                    echo "<option value=\"". $row["rum"] ."\" selected=\"selected\">". $row["rum"] ."</option>";
+                  } else {
+                    echo "<option value=\"". $row["rum"] ."\">". $row["rum"] ."</option>";
+                  }
+                }
+              ?>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label for="objekttyp"><span>Objekttyp</span>
+            <select id="objekttyp" name="objekttyp">
+              <option value=""></option>
+              <?php 
+                $object_type->data_seek(0);
+                while($row = $object_type->fetch_assoc()) {
+                  if($stored_search_params['objekttyp'] == $row['objekttyp']) {
+                    echo "<option value=\"". $row["objekttyp"] ."\" selected=\"selected\">". $row["objekttyp"] ."</option>";
+                  } else {
+                    echo "<option value=\"". $row["objekttyp"] ."\">". $row["objekttyp"] ."</option>";
+                  }
+                }
+              ?>
+            </select>
+          </label>
+        </p>
+
+        <input type="submit" value="Sök" id="search">
+      </form>
+    </div>
+    <div id="search_results"></div>
+    <script type="text/javascript">
+      var performAjaxRequest = function() {
+        $.ajax({
+          type: "GET",
+          url: "result.php",
+          data: $('#search_form').serialize(),
+          success: function (data) {
+            $('#search_results').html(data);
+          }
+        });
+      };
+      $('#search').click(function(){
+        performAjaxRequest();
+        return false;
+      });
+      performAjaxRequest();
+    </script>
+  </body>
+</html>
+
+<?php
+  $mysqli->close();
+?>
+
